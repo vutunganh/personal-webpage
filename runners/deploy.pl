2ceb2468804d1de007257f5bd61a6ec2e03f38a1
Tung Anh Vu <vu.tunganh96@gmail.com>
2018-10-01 22:56:26 +0200

    each page can have its own h1 title

--- runners/deploy.pl
+++ runners/deploy.pl
@@ -27,18 +27,43 @@ my $site_header = undef;
   $site_header = <$fh>
 }
 
+sub header_info
+{
+  my $stream = $_[0];
+  my %to_return;
+  while (<$stream>)
+  {
+    last if ($_ =~ /\<\!DOCTYPE html\>/);
+    my ($key, $value) = $_ =~ /(\S+)\s*(.*)/;
+    $to_return{$key} = $value;
+  }
+
+  return \%to_return;
+}
+
 foreach (@html_files) {
   my $fn = $_;
   my $bk = $fn . ".bak";
   rename $fn, $bk;
   open my $in, '<', $bk or die "Couldn't open '$bk'.\n";
   open my $out, '>>', $fn or die "Couldn't open '$fn'.\n";
+
+  my $cur_head = $head;
+  my $cur_site_header = $site_header;
+
+  my $hinfo = header_info $in;
+  my $page_title_placeholder = '<!-- <h1>__PAGE_TITLE__</h1> -->';
+  if (exists $hinfo->{PAGE_TITLE}) {
+    $cur_site_header =~ s/$page_title_placeholder/<h1>$hinfo->{PAGE_TITLE}<\/h1>/;
+  } else {
+    $cur_site_header =~ s/$page_title_placeholder/<h1>Homepage of Tung Anh Vu<\/h1>/;
+  }
+  
   while (<$in>) {
-    $_ =~ s/<!-- __HEAD__ -->/$head/;
-    $_ =~ s/<!-- __SITE_HEADER__ -->/$site_header/;
+    $_ =~ s/<!-- __HEAD__ -->/$cur_head/;
+    $_ =~ s/<!-- __SITE_HEADER__ -->/$cur_site_header/;
     $_ =~ s/<!-- .*? -->//g;
     print $out $_;
-    # print STDERR $_;
   }
 }
 
